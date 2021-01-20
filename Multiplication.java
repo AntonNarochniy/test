7c894fc4bc3aecc5ef056c34181fd9664248c5cf
AntonNarochniy <test.788test788@gmail.com>
2021-01-20 19:15:33 +0600

    Initial commit

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String[] args) {
+        System.out.print("Ведите число от 0 до 11: ");
+        Scanner scanner = new Scanner(System.in);
+        int number =  scanner.nextInt();
+       if (number < 0 || number > 11) {
+           System.out.println("Введено отрицательное число или число больше 11");
+           System. exit(0);
+       }
+	   for(int i = 0; i <= 11; i++) {
+		   System.out.println(number + " x " + i + " = " + (number * i));
+	   }   
+	}
+}
